61946caed40e61a5777fad7072044d71df65cae0
Dalmo Pereira <dalmossa.dev@gmail.com>
2026-03-20 01:06:05 -0300

    Delete src directory

--- src/modules/admin/dto/create-barbershop.dto.ts
+++
@@ -1,9 +0,0 @@
-// src/modules/admin/dto/create-barbershop.dto.ts
-
-export interface AdminCreateBarbershopDTO {
-  name: string
-  address: string
-  phones: string
-  description: string
-  imageUrl: string
-}
--- src/modules/admin/dto/create-service.dto.ts
+++
@@ -1,8 +0,0 @@
-// src/modules/admin/dto/create-service.dto.ts
-
-export interface AdminCreateServiceDTO {
-  name: string
-  description: string
-  imageUrl: string
-  price: number
-}
